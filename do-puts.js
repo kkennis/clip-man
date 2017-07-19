5ba85ccae12a90318f7cc014722ca819463f61fc
Kevin Kennis <kevinvkennis@gmail.com>
2017-07-19 00:39:39 -0700

    resolve conflicts

--- do-puts.js
+++ do-puts.js
@@ -1,32 +1,14 @@
 var puts = {
     "hello": "goodbye",
-<<<<<<< HEAD
     "secret code": "up up down down left right left right b a",
-    "greeting": "Well howdy there pard'na!!"
-=======
+    "greeting": "Well howdy there pard'na!!",
     "hola": "adios",
     "bonjour": "au revior",
->>>>>>> react
 }
 
 
 
 function doPuts(db) {
-<<<<<<< HEAD
-    var putPs = Object.keys(puts).map((put) =>
-        db.put({
-            _id: new Date().toJSON(),
-            key: put,
-            value: puts[put]
-        });
-    )
-=======
-    window.DB = db;
-    window.doPuts = doPuts;
->>>>>>> react
-
-    console.log("About to destroy")
-    // db.destroy()
     Promise.resolve()
         .then(() => Promise.all(Object.keys(puts).map((put) =>
             db.put({
