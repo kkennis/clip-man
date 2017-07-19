9fd47fe8674316e50a9d78d81ea761fbf09ebf54
Kevin Kennis <kevinvkennis@gmail.com>
2017-07-18 18:51:43 -0700

    move elements in index.html into app component

---
+++ client/components/app.js
@@ -0,0 +1,9 @@
+const React = require('react');
+
+const App = () => (
+    
+
+
+);
+
+module.exports = App;
--- clipman.js
+++ clipman.js
@@ -1,140 +1,144 @@
-const $ = require('./lib/jquery.min.js');
-PouchDB.plugin(require('pouchdb-legacy-utils'));
-PouchDB.plugin(require('pouchdb-find'));
-const db = new PouchDB('clips');
-const copy = require('copy-to-clipboard');
-const ipcRenderer = require('electron').ipcRenderer;
+const ReactDOM = require('react-dom');
+const React = require('react');
+const App = require('./components/app');
 
-let currentClips;
+// PouchDB.plugin(require('pouchdb-legacy-utils'));
+// PouchDB.plugin(require('pouchdb-find'));
+// const db = new PouchDB('clips');
 
-$(document).ready(function() {
-    // db.createIndex({
-    //     index: { fields: ['key'] }
-    // })
-    Promise.resolve()
-        .then(init);
-});
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
-function init() {
-    populateKeys();
 
-    let $search = $('#key-input');
-    $search.focus();
-    $search.on('keyup', updateResults)
+// const copy = require('copy-to-clipboard');
+// const ipcRenderer = require('electron').ipcRenderer;
 
-    $('.clip-container').on('keyup', '.item', handleItemClick)
-    $('.add-val').on('click', showAdd)
-    $('.add-item').on('keyup', '#new-val-input, #new-key-input', handleInputHotkey)
-}
+// let currentClips;
 
-function populateKeys() {
-    db.allDocs({ include_docs: true })
-        .then(drawInitial)
-}
+// function init() {
+//     populateKeys();
 
-function updateResults(event) {
-    if (event.ctrlKey && event.keyCode === 78) {
-        showAdd();
-    } else if (event.keyCode === 40) {
-        $('.item:first-child').focus();
-    } else {
-        let inputVal = $(event.target).val();
+//     let $search = $('#key-input');
+//     $search.focus();
+//     $search.on('keyup', updateResults)
 
-        db.find({
-            selector: {
-                key: {
-                    $regex: inputVal
-                }
-            }
-        })
-        .then(drawResults);
-    }
-}
+//     $('.clip-container').on('keyup', '.item', handleItemClick)
+//     $('.add-val').on('click', showAdd)
+//     $('.add-item').on('keyup', '#new-val-input, #new-key-input', handleInputHotkey)
+// }
 
-function drawResults(results) {
-    currentClips = results.docs
-    drawClips();
-}
+// function populateKeys() {
+//     db.allDocs({ include_docs: true })
+//         .then(drawInitial)
+// }
 
-function drawInitial(results) {
-    if (results.total_rows > 0) {
-        currentClips = results.rows.map((r) => r.doc);
-        drawClips();
-    }
-}
+// function updateResults(event) {
+//     if (event.ctrlKey && event.keyCode === 78) {
+//         showAdd();
+//     } else if (event.keyCode === 40) {
+//         $('.item:first-child').focus();
+//     } else {
+//         let inputVal = $(event.target).val();
 
-function drawClips() {
-    $('.clip-container').html('');
-    currentClips.reverse().forEach((item) => {
-        $('.clip-container').append(itemTemplate(item));
-    });
-}
+//         db.find({
+//             selector: {
+//                 key: {
+//                     $regex: inputVal
+//                 }
+//             }
+//         })
+//         .then(drawResults);
+//     }
+// }
 
-function itemTemplate(item) {
-    return `
-<div class='item' tabindex='1' _id=${item._id}>
-    <h4 class='item-header'>${item.key}</h4>
-    <p class='item-value'>${item.value}</p>
-</div>`;
-}
+// function drawResults(results) {
+//     currentClips = results.docs
+//     drawClips();
+// }
 
-function handleItemClick(event) {
-    if (event.ctrlKey && event.keyCode === 78) {
-        showAdd();
-    } else if (event.keyCode === 13) {
-        const value = $(event.target).find('.item-value').html();
-        copy(value);
-        $(event.target).blur();
-        console.log('Sending message!');
-        ipcRenderer.send('selected');
-    } else if (event.keyCode === 38) {
-        if ($(event.target).prev().length > 0) {
-            $(event.target).prev().focus();
-        } else {
-            $('#key-input').focus();
-        }
-    } else if (event.keyCode === 40) {
-        $(event.target).next().focus();
-    } else if (event.keyCode === 27) {
-        $('#key-input').focus();
-    } else if (event.ctrlKey && event.keyCode == 68) {
-        const id = $(event.target).attr('_id');
+// function drawInitial(results) {
+//     if (results.total_rows > 0) {
+//         currentClips = results.rows.map((r) => r.doc);
+//         drawClips();
+//     }
+// }
 
-        const doc = currentClips.find((clip) => clip._id === id);
-        db.remove(doc._id, doc._rev).then(populateKeys);
-    }
-}
+// function drawClips() {
+//     $('.clip-container').html('');
+//     currentClips.reverse().forEach((item) => {
+//         $('.clip-container').append(itemTemplate(item));
+//     });
+// }
 
-function showAdd(event) {
-    $('.input-line').hide()
-    $('.add-item').show()
-    $('#new-key-input').focus();
-}
+// function itemTemplate(item) {
+//     return `
+// <div class='item' tabindex='1' _id=${item._id}>
+//     <h4 class='item-header'>${item.key}</h4>
+//     <p class='item-value'>${item.value}</p>
+// </div>`;
+// }
 
-function showSearch(event) {
-    $('.input-line').show()
-    $('.add-item').hide()
-    $('#key-input').focus()
-}
+// function handleItemClick(event) {
+//     if (event.ctrlKey && event.keyCode === 78) {
+//         showAdd();
+//     } else if (event.keyCode === 13) {
+//         const value = $(event.target).find('.item-value').html();
+//         copy(value);
+//         $(event.target).blur();
+//         console.log('Sending message!');
+//         ipcRenderer.send('selected');
+//     } else if (event.keyCode === 38) {
+//         if ($(event.target).prev().length > 0) {
+//             $(event.target).prev().focus();
+//         } else {
+//             $('#key-input').focus();
+//         }
+//     } else if (event.keyCode === 40) {
+//         $(event.target).next().focus();
+//     } else if (event.keyCode === 27) {
+//         $('#key-input').focus();
+//     } else if (event.ctrlKey && event.keyCode == 68) {
+//         const id = $(event.target).attr('_id');
 
-function handleInputHotkey(event) {
-    event.preventDefault();
+//         const doc = currentClips.find((clip) => clip._id === id);
+//         db.remove(doc._id, doc._rev).then(populateKeys);
+//     }
+// }
 
-    if (event.keyCode === 27) {
-        showSearch();
-    } else if (event.ctrlKey && event.keyCode === 13) {
-        let key = $('[name=key]').val()
-        let value = $('[name=value]').val()
-        $('[name=key]').val('')
-        $('[name=value]').val('').html('')
+// function showAdd(event) {
+//     $('.input-line').hide()
+//     $('.add-item').show()
+//     $('#new-key-input').focus();
+// }
 
-        db.put({
-            _id: new Date().toJSON(),
-            key,
-            value
-        }).then(() => {
-            populateKeys();
-            showSearch();
-        })
-    }
-}
+// function showSearch(event) {
+//     $('.input-line').show()
+//     $('.add-item').hide()
+//     $('#key-input').focus()
+// }
+
+// function handleInputHotkey(event) {
+//     event.preventDefault();
+
+//     if (event.keyCode === 27) {
+//         showSearch();
+//     } else if (event.ctrlKey && event.keyCode === 13) {
+//         let key = $('[name=key]').val()
+//         let value = $('[name=value]').val()
+//         $('[name=key]').val('')
+//         $('[name=value]').val('').html('')
+
+//         db.put({
+//             _id: new Date().toJSON(),
+//             key,
+//             value
+//         }).then(() => {
+//             populateKeys();
+//             showSearch();
+//         })
+//     }
+// }
